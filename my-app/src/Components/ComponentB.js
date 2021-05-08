e76e910f5c37178f380d2e0749dd3cd9bf17977b
cheseryon050822 <cseryon@gmail.com>
2021-05-08 17:16:56 +0900

    Teruaki Sato

--- my-app/src/Components/ComponentB.js
+++ my-app/src/Components/ComponentB.js
@@ -1,23 +1,107 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch,useHistory} from 'react-router-dom';
+import React,{useReducer,useState} from 'react';
+import {Link} from 'react-router-dom';
+import {ADD_EVENT, DELETE_ALL_EVENT}from '../actions/index';
+import reducer from '../reducer/index';
+import {Button,Form,Table}from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const ComponentB = () =>{
-    const history= useHistory()
+    const [state,dispatch]=useReducer(reducer,[]);
+    const [title,setTitle]=useState('');
+    const [body,setBody]=useState('');
+    
+    const handleClick = (e)=>{
+        e.preventDefault();
+        dispatch({
+            type:ADD_EVENT,
+            title,
+            body
+        });
+        setTitle('');
+        setBody('');
+    };
+
+    const FukuokaDaieiHowks = (f)=>{
+        f.preventDefault();
+        dispatch({
+            type:DELETE_ALL_EVENT,
+            title,
+            body
+        });
+        setTitle('');
+        setBody('');
+    };
+
+
     const joumon = () => {
-        history.push("/")
+    ("/")
         }
     const  heian= () => {
-        history.push("/ComponentC")
+    ("/ComponentC")
     }
+    
+    
     return(
-　　<Router>
-      <Switch>
+        <div>
+            <div>ComponentB</div>
+            <Link to="/componentc">ComponentCへ移動</Link>
+       <Form>
+        <Form.Group control="fromBasicPassword">
+        <Form.Label>Title</Form.Label>
+            <Form.Control
+                type="text"
+                pleceholder="title"
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}/>
+            <Form.Label>Body</Form.Label>
+            <Form.Control
+                type="text"
+                pleceholder="body"
+                value={body}
+                onChange={(e)=>setBody(e.target.value)} />
+        </Form.Group>
+        <Button variant="danger" onClick={handleClick}>
+            イベント作成
+        </Button>
+        <Button variant="primary" onClick={FukuokaDaieiHowks}>
+            イベント全削除
+        </Button> 
+        </Form>
+       
+        
+      
       <div>
       <button onClick={joumon}>ComponentAへ移動</button>
       <button onClick={heian}>ComponentCへ移動</button>  
         </div>
-        </Switch>
-     </Router>
+        <h1>Table</h1>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>body</th>
+                    <th>#</th>
+                </tr>
+            </thead>
+            <tbody>
+                {state.map((data,index)=>{
+                    return(
+                        <tr key={index}>
+                            <td>{data.id}</td>
+                            <td>{data.body}</td>
+                            <td>{data.title}</td>
+                            <td>
+                             <Button variant="danger">削除</Button>
+                            </td>
+                        </tr>
+                    );
+                })}             
+            </tbody>
+        </Table>
+        </div>
+     
     );
 };
 
--- my-app/src/reducer/index.js
+++ my-app/src/reducer/index.js
@@ -1,9 +1,19 @@
+import {ADD_EVENT,DELETE_ALL_EVENT} from '../actions/index';
+
 const reducer = (state=[],action)=>{
-    switch (action.type){
-        case 'INCREMENT':
-            return{...state,count:state.count+1};
-            default:
-                return state;
+    switch (action.type) {
+    case ADD_EVENT:
+        const event={title:action.title,body:action.title};
+        const id = state.length + 1
+        console.log(id)
+        return [...state,{id,...event}];
+    case DELETE_ALL_EVENT:
+        const Nankai={title:action.title,body:action.title};
+        const di = state.length  - 
+        console.log(di)
+        return [...state,{di,...Nankai}];
+    default:
+        return state;
 
     }
 };
